6d0a60473dbc9279f66d0ffd5d5ca9ef20f86dc1
rickmurdock <rick.murdock@live.com>
2017-09-18 09:17:29 -0500

    Changed newsletters from data files to google drive.

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -1,32 +1,28 @@
 import React, { Component } from 'react';
-import NewsletterCard from './NewsletterCard';
-import newsletters from '../data/newsletters';
+import GoogleDriveIcon from '../images/GoogleDriveIcon.png';
 
 class Newsletter extends Component {
   render() {
-    const uniqueYears = [...new Set(newsletters.map(item => item.year))];
-    
-    const newsYears = uniqueYears.map((year, index) => {
-      let lettersByYear = newsletters.filter(letter => letter.year === year);
-      return (
-        <NewsletterCard key={index} year={year} letters={lettersByYear} />
-      );
-    });
     return (
       <div className="col-md-12 newsletter-page">
         <div className="container newsletter-container">
           <h2 className="text-center page-title-newsletter my-3">The Flatwheel Newsletter</h2>
-          <p className="page-intro-newsletter">
-            The first newsletter was called the <i>Orange Peel</i>; it was published occasionally by the President. 
-            Publication of the <i>Orange Peel</i> began in 1971.
-            <br/><br/>
-            In October 1975, the first <i>Flatwheel</i> newsletter (Vol. 1, Number 1) was published. With few exceptions, 
-            the newsletter has been published monthly and continues today. <i>The Flatwheel</i> keeps Society members and 
-            friends informed of upcoming meetings and activities and the latest in local and national railroading activities.
-          </p>
-
-          <div className="card-deck">
-            {newsYears}
+          <div className="page-intro-newsletter">
+            <p>
+              The first newsletter was called the <i>Orange Peel</i>; it was published occasionally by the President. 
+              Publication of the <i>Orange Peel</i> began in 1971.
+            </p>
+            <p>
+              In October 1975, the first <i>Flatwheel</i> newsletter (Vol. 1, Number 1) was published. With few exceptions, 
+              the newsletter has been published monthly and continues today. <i>The Flatwheel</i> keeps Society members and 
+              friends informed of upcoming meetings and activities and the latest in local and national railroading activities.
+            </p>
+            <p>
+              Current and past issues of <i>The Flatwheel</i> are available on Google Drive™.
+            </p>
+            <span className="flatwheel-link"> 
+              <a href="https://drive.google.com/drive/folders/0BxsaKH9lyrNwa3ZrcVZXNmNsX1k?usp=sharing" target="_blank" rel="noopener noreferrer"><h5><img className="drive-img" src={GoogleDriveIcon} alt="Color Google Drive Icon"/> &nbsp;View <i>The FLATWHEEL</i></h5></a>
+            </span>
           </div>
         </div>
       </div>
