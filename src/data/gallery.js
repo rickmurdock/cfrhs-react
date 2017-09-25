f0185c124f61266e26512087c7f67edf80a0f56a
rickmurdock <rick.murdock@live.com>
2017-09-24 20:47:07 -0500

    Added more images for gallery and museum pages.

--- src/data/gallery.js
+++ src/data/gallery.js
@@ -1,19 +1,19 @@
 const gallery = [
   {
-    imageName: "CRRCaboose.jpg",
+    imageName: "Caboose.jpg",
     caption: "Behind the Central Florida Railroad Museum is a Clinchfield caboose, which is available for exploring.",
     type: "",
   },
- {
-    imageName: "ChessieCaboose.jpg",
-    caption: "This Chessie caboose is located at the nearby Winter Garden Heritage Museum.",
+  {
+    imageName: "FairmontMotorcar.jpg",
+    caption: "1938 Fairmont motorcar",
     type: "",
   },
   {
     imageName: "SteamRun.jpg",
     caption: "The Tavares, Eustis and Gulf Railroad brought its steam locomotive to the museum several years ago for special passenger runs.",
     type: "",
-    },
+  },
   {
     imageName: "Plaque.jpg",
     caption: "This historic plaque is located at the entrance to the Railroad Museum.",
@@ -27,6 +27,11 @@ const gallery = [
   {
     imageName: "OrangeBeltRR.jpg",
     caption: "An Orange Belt train, which had a Winter Garden station, is pictured in the late 1890s.",
+    type: "",
+  },
+  {
+    imageName: "ChessieCaboose.jpg",
+    caption: "This Chessie caboose is located at the nearby Winter Garden Heritage Museum.",
     type: "",
   },
 ]
