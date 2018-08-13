01e5ec28c3fbf01198462acc186336c0ca70bf0e
rickmurdock <rick.murdock@live.com>
2018-08-12 19:46:44 -0500

    Added paypal links on membership page.

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -24,6 +24,12 @@ const Membership = () => {
                     </div>
                     <div className="card-footer text-center">
                       <h6 className="my-auto member-card-footer">{level.cost}</h6>
+                      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                        <input type="hidden" name="cmd" value="_s-xclick" />
+                        <input type="hidden" name="hosted_button_id" value={level.paypalValue} />
+                        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+                        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                      </form>
                     </div>
                   </div>
                 </div>
--- src/data/membership.js
+++ src/data/membership.js
@@ -3,47 +3,56 @@ const membership = [
   {
     level: "Society Member",
     description: "Any person of good character, eighteen (18) years of age or older, and interested in any/all aspects of railroading and the Purpose and  Mission Statement of this Society shall be eligible for membership. Society members have the right to vote and hold office.",
-    cost: "$45/year"
+    cost: "$45/year",
+    paypalValue: "F2B9XWGWAZ6TG"
   },
   {
     level: "Society Friend",
     description: "Any person of good character, eighteen (18) years of age or older, interested in any/all aspects of railroading and the Purpose and Mission Statement of this Society, and does not wish to have the right to vote or hold office in the Society, shall be eligible for Society Friend membership.",
-    cost: "$25/year"
+    cost: "$25/year",
+    paypalValue: "MWJ4HWRWJ42YU"
   },
   {
     level: "Society Student Member",
     description: "Any person under the age of eighteen (18) years old interested in any/all aspects of railroading and the Purpose and Mission Statement of this Society shall be eligible for membership, but will not have the right to vote or hold office in the Society.",
-    cost: "$15/year"
+    cost: "$15/year",
+    paypalValue: "FD8UYQ73BENWG"
   },
   {
     level: "Society Family Member",
     description: "Society Family membership shall be available for up to six (6) family members of a Society Member. Family members will not have the right to vote or hold office in the Society.",
-    cost: "$10/year"
+    cost: "$10/year",
+    paypalValue: "FA73AJDXWEMAU"
   },
   {
     level: "Society Century Member",
     description: "Any person of good character, eighteen (18) years of age or older, and interested in any/all aspects of railroading and the Purpose and Mission Statement of this Society shall be eligible for Society Century Membership.  Society Century Members have the right to vote and hold office.",
-    cost: "$100/year"
+    cost: "$100/year",
+    paypalValue: "88WZP3GG7VC6J"
   },
   {
     level: "Bronze Corporate Member",
     description: "Any corporation interested in supporting any/all aspects of railroading and the Purpose and Mission Statement of this Society at the Bronze Level shall be eligible for Corporate Membership. Dues for Corporate Members shall be determined by the Society’s Board of Directors. Corporate Members will not have the right to vote or hold office.",
-    cost: "$250/year"
+    cost: "$250/year",
+    paypalValue: "W9HRVMT3W5VR2"
   },
   {
     level: "Gold Corporate Member",
     description: "Any corporation interested in supporting any/all aspects of railroading and the Purpose and Mission Statement of this Society at the Bronze Level shall be eligible for Corporate Membership. Dues for Corporate Members shall be determined by the Society’s Board Directors. Corporate Members will not have the right to vote or hold office.",
-    cost: "$500/year"
+    cost: "$500/year",
+    paypalValue: "5EWPTCYYSTPWQ"
   },
   {
     level: "Diamond Corporate Member",
     description: "Any corporation interested in supporting any/all aspects of railroading and the Purpose and Mission Statement of this Society at the Bronze Level shall be eligible for Corporate Membership. Dues for Corporate Members shall be determined by the Society’s Board of Directors. Corporate Members will not have the right to vote or hold office.",
-    cost: "$750/year"
+    cost: "$750/year",
+    paypalValue: "M9NG292497SNS"
   },
   {
     level: "Platinum Corporate Member",
     description: "Any corporation interested in supporting any/all aspects of railroading and the Purpose and Mission Statement of this Society at the Bronze Level shall be eligible for Corporate Membership. Dues for Corporate Members shall be determined by the Society’s Board of Directors. Corporate Members will not have the right to vote or hold office.",
-    cost: "$1,000/year"
+    cost: "$1,000/year",
+    paypalValue: "9SJBDC5F2REKQ"
   }
 ]
 
