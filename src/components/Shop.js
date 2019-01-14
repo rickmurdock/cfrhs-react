29929bffefa58b85dccaecf9541f8783ca4be729
rickmurdock <rick.murdock@live.com>
2019-01-13 20:21:27 -0600

    Removed link to ebay store.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -56,9 +56,9 @@ const Shop = () => {
             <div className="card-header text-center align-middle">
               <a className="order-link" href="https://drive.google.com/file/d/1hnU5XFMnK5Veg53H8XlHdAI_qGnQHUvb/view?usp=sharing" target="_blank" rel="noopener noreferrer"><i className="fa fa-file-pdf-o pdf" aria-hidden="true">&nbsp;</i>Order Form</a>
             </div>
-            <div className="card-block text-center">
+            {/* <div className="card-block text-center">
               <p>Additional items available on our <br /><a className="text-center" href="http://stores.ebay.com/CFRHS-MUSEUM-STORE" target="_blank" rel="noopener noreferrer">eBay Store</a></p>
-            </div>
+            </div> */}
           </div>
         </div>
 
